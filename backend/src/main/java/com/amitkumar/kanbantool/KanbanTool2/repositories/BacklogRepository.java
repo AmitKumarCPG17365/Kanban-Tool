342ea043d2df9b8b4d50db765e4f0fd13936649d
AmitKumarCPG17365 <90821170+AmitKumarCPG17365@users.noreply.github.com>
2021-11-03 10:16:17 +0530

    Add files via upload

---
+++ backend/src/main/java/com/amitkumar/kanbantool/KanbanTool2/repositories/BacklogRepository.java
@@ -0,0 +1,13 @@
+package com.amitkumar.kanbantool.KanbanTool2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amitkumar.kanbantool.KanbanTool2.domain.Backlog;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long>{
+	
+	Backlog findByProjectIdentifier(String Identifier);
+
+}
---
+++ backend/src/main/java/com/amitkumar/kanbantool/KanbanTool2/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.amitkumar.kanbantool.KanbanTool2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amitkumar.kanbantool.KanbanTool2.domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+
+	User findByUsername(String username);
+	User getById(Long id);
+	
+}
